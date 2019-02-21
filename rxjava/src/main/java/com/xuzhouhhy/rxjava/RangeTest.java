63b3d8080a0108f03976b1f46d5570a6fd29d302
hanhongyun <henry.han@ximalaya.com>
2019-02-21 16:07:32 +0800

    add interval range operator test

--- rxjava/src/main/java/com/xuzhouhhy/rxjava/RangeTest.java
+++ rxjava/src/main/java/com/xuzhouhhy/rxjava/RangeTest.java
@@ -1,5 +1,9 @@
 package com.xuzhouhhy.rxjava;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
@@ -14,9 +18,23 @@ public class RangeTest {
                 .subscribe(new Consumer<Integer>() {
                     @Override
                     public void accept(Integer integer) throws Exception {
-                        System.out.println(integer);
+                        System.out.println(SimpleDateFormat.getTimeInstance().format(new Date()) + " value:" + integer);
                     }
                 });
+
+        Disposable subscribe1 = Observable.intervalRange(2, 10, 2, 1, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        System.out.println(SimpleDateFormat.getTimeInstance().format(new Date()) + " value:" + aLong);
+                    }
+                });
+
+        try {
+            Thread.sleep(20 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
