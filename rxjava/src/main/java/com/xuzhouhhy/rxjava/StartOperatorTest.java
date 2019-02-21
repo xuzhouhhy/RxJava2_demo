070a60cc8914f59baa6e3a07ed680ce68a7203eb
hanhongyun <henry.han@ximalaya.com>
2019-02-21 16:24:06 +0800

    add start operator test

---
+++ rxjava/src/main/java/com/xuzhouhhy/rxjava/StartOperatorTest.java
@@ -0,0 +1,14 @@
+package com.xuzhouhhy.rxjava;
+
+import io.reactivex.Observable;
+
+/**
+ * created by hanhongyun on 2019/2/21 16:22
+ */
+public class StartOperatorTest {
+
+    public static void main(String... args){
+        //start in rxjava2 is renamed to fromCallable
+    }
+
+}
